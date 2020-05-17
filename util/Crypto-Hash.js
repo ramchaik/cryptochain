96df07c88dd077cd87c09d0b88acfd4d5d656140
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-05-17 23:05:51 +0530

    [XXX]: add transaction's update()

--- util/Crypto-Hash.js
+++ util/Crypto-Hash.js
@@ -1,10 +1,15 @@
-const crypto = require('crypto');
+const crypto = require("crypto");
 
 const cryptoHash = (...inputs) => {
-  const hash = crypto.createHash('sha256');
-  hash.update(inputs.sort().join(' '));
+  const hash = crypto.createHash("sha256");
+  hash.update(
+    inputs
+      .map((input) => JSON.stringify(input))
+      .sort()
+      .join(" ")
+  );
 
-  return hash.digest('hex');
+  return hash.digest("hex");
 };
 
 module.exports = cryptoHash;
