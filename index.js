edb09e72c2e4c80e6ec5ffd4b52bec8cd364ab4b
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-05-18 23:35:45 +0530

    add transaction pool in pubsub network

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
-const pubsub = new PubSub({ blockchain });
+const pubsub = new PubSub({ blockchain, transactionPool });
 
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
@@ -53,6 +53,8 @@ app.post("/api/transact", (req, res) => {
 
   transactionPool.setTransaction(transaction);
 
+  pubsub.broadcaseTransaction(transaction);
+
   res.json({ type: "success", transaction });
 });
 
@@ -60,7 +62,7 @@ app.get("/api/transaction-pool-map", (req, res) => {
   return res.json(transactionPool.transactionMap);
 });
 
-const syncChains = () => {
+const syncWithRootState = () => {
   request(
     { url: `${ROOT_NODE_ADDRESS}/api/blocks` },
     (error, response, body) => {
@@ -69,6 +71,22 @@ const syncChains = () => {
 
         console.log("replace chain on a sync with", rootChain);
         blockchain.replaceChain(rootChain);
+      }
+    }
+  );
+
+  request(
+    { url: `${ROOT_NODE_ADDRESS}/api/transaction-pool-map` },
+    (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const rootTransactionPoolMap = JSON.parse(body);
+
+        console.log(
+          "replace transactio pool map on a sync with",
+          rootTransactionPoolMap
+        );
+
+        transactionPool.setMap(rootTransactionPoolMap);
       }
     }
   );
@@ -86,6 +104,6 @@ app.listen(PORT, () => {
   console.log(`listening at localhost:${PORT}`);
 
   if (PORT !== DEFAULT_PORT) {
-    syncChains();
+    syncWithRootState();
   }
 });
