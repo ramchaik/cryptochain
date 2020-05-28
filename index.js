5998ff0504aa1c12637080990dd3fe4951501531
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-05-29 01:01:36 +0530

    Setup React

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const path = require("path");
 const Blockchain = require("./Blockchain");
 const TransactionPool = require("./wallet/transaction-pool");
 const Wallet = require("./wallet/index");
@@ -23,6 +24,7 @@ const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "client/dist")));
 
 app.get("/api/blocks", (req, res) => {
   res.json(blockchain.chain);
@@ -89,6 +91,10 @@ app.get("/api/wallet-info", (req, res) => {
       address,
     }),
   });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/dist/index.html"));
 });
 
 const syncWithRootState = () => {
